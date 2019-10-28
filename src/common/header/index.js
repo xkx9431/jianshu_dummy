59865f609643a799d18a200c54c0235ace6d9ef8
Kaixuan Xu <triumph_9431@qq.com>
2019-10-28 23:49:11 +0800

    add header component  statics

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,10 +1,25 @@
-import React,{ Component } from 'react'
-import { HeaderWrapper } from './style'
+import React, { Component } from 'react'
+import { HeaderWrapper, Logo, Nav,
+        NavItem, NavSearch, Addition, Button
+} from './style'
 
 class Header extends Component {
     render(){
         return (
-            <HeaderWrapper>Header</HeaderWrapper>
+            <HeaderWrapper>
+                <Logo />
+                <Nav>
+                    <NavItem className='left'>首页</NavItem>
+                    <NavItem className='left'>下载App</NavItem>
+                    <NavItem className='right'>登录</NavItem>
+                    <NavItem className='right'>Aa</NavItem>
+                    <NavSearch/>
+                </Nav>
+                <Addition>
+                    <Button className='reg'>写文章</Button>
+                    <Button className='writing'>注册</Button>
+                </Addition>
+            </HeaderWrapper>
         )
     }
 }

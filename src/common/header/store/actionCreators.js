5baf68f859d882f1eca198e7fa280586c357fcec
Kevin, Xu <kaixuan.xu@siemens.com>
2019-10-29 16:12:23 +0800

    add async function for get listitem

--- src/common/header/store/actionCreators.js
+++ src/common/header/store/actionCreators.js
@@ -1,4 +1,11 @@
 import * as constants from './constants';
+import { fromJS } from 'immutable';
+import axios from 'axios';
+
+const changeList = (data) => ({
+	type: constants.CHANGE_LIST,
+	data: fromJS(data)
+});
 
 export const searchFocus = () => ({
 	type: constants.SEARCH_FOCUS
@@ -6,4 +13,15 @@ export const searchFocus = () => ({
 
 export const searchBlur = () => ({
 	type: constants.SEARCH_BLUR
-})
+});
+
+export const getList = () => {
+	return (dispatch) => {
+		axios.get('/api/headerList.json').then((res) => {
+			const data = res.data;
+			dispatch(changeList(data.data));
+		}).catch(() => {
+			console.log('error');
+		})
+	}
+};
--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,16 +1,20 @@
 import * as constants from './constants';
-import { fromJS } from 'immutable'
+import { fromJS } from 'immutable';
 
-const defaultState =fromJS({
-	focused: false
+const defaultState = fromJS({
+	focused: false,
+	list: []
 });
 
 export default (state = defaultState, action) => {
-	if (action.type === constants.SEARCH_FOCUS) {
-		return state.set('focused',true)
+	switch(action.type) {
+		case constants.SEARCH_FOCUS:
+			return state.set('focused', true);
+		case constants.SEARCH_BLUR:
+			return state.set('focused', false);
+		case constants.CHANGE_LIST:
+			return state.set('list', action.data);
+		default:
+			return state;
 	}
-	if (action.type === constants.SEARCH_BLUR) {
-		return state.set('focused',false)
-	}
-	return state;
 }

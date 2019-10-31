86a3a476fcd8ce2c11d3c6212442518774dd0f20
Kevin, Xu <kaixuan.xu@siemens.com>
2019-10-31 14:25:44 +0800

    add some refine for info background color

--- src/common/header/style.js
+++ src/common/header/style.js
@@ -105,6 +105,8 @@ export const SearchInfo = styled.div`
 	width: 240px;
 	padding: 0 20px;
 	box-shadow: 0 0 8px rgba(0, 0, 0, .2);
+	background:rgb(255, 255, 255);
+	color:#000;
 `;
 
 export const SearchInfoTitle = styled.div`

0f07d1349fb09892b949d2db5edcb0f38a8a5ef3
Kevin, Xu <kaixuan.xu@siemens.com>
2019-11-04 14:54:54 +0800

    add fetch id function

--- src/pages/details/index.js
+++ src/pages/details/index.js
@@ -30,8 +30,8 @@ const mapState = (state) => ({
 	content: state.getIn(['detail', 'content'])
 });
 const mapDispatch = (dispatch) =>({
-    getDetail() {
-        dispatch(actionCreators.getDetail());
+    getDetail(id) {
+        dispatch(actionCreators.getDetail(id));
     }
 })
 
